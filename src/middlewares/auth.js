aff7a5ffae63ad398f1076e578cab2770dcbaddd
Wasswa Jim Daniels <wasswa.jim@warchild.nl>
2021-08-13 13:46:15 +0300

    Modified Auth middleware implemented JWT

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,24 +1,27 @@
 const jwt = require('jsonwebtoken');
+const User = require('../db/models/User');
 
-const auth_middleware = (req, res, next) => {
-    console.log('lllll\n Here \nllllll');
-    // try {
-    // const decoded = jwt.verify(req.token, 'beefup-secret-key');
-    // const user = User.findOne({ _id: decoded._id, 'tokens.token': req.token });
-    // if (!user) {
-    //     throw new Error();
-    // }
-    // req.auth_user = user;
-    if (req.isAuthenticated()) { 
-        return next();
-    }
-    req.session.error = 'Please sign in!';
-    // if(req.user.authenticated){
-    //     next();
-    // }
-    res.redirect('/login/');
+const auth_middleware = async(req, res, next) => {
+    // try {        
+        // const token =  req.headers.authorization.split(' ')[1];
+        const token = req.cookies.token;
+        const decoded = jwt.verify(token, 'beefup-secret-key');
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+        if (!user) {
+            throw new Error();
+        }
+        req.user = user;
+        next();
+        // if (req.isAuthenticated()) { 
+        //     return next();
+        // }
+        // req.session.error = 'Please sign in!';
+        // if(req.user.authenticated){
+        //     next();
+        // }
+        // res.redirect('/login/');
     // } catch (e) {
-    //     res.status(401).send('Login Required.');
+    //     res.status(401).json('Login Required.');
     // }
 };
 
