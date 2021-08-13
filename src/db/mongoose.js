c19241bf893c7bf5a21f81d6f0e5f596f04318ef
Wasswa Jim Daniels <wasswa.jim@warchild.nl>
2021-08-13 15:19:17 +0300

    Debug heroku

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,14 +1,26 @@
 const mongoose = require('mongoose');
 const chalk = require('chalk');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/beefup-db', {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}, (error) => {
-    if(error){
-        chalk.red('Failed to connect to the database. Please try again later.')
-    } else {
-        chalk.green('Connection Successful.');
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/beefup-db', {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+    } catch (e) {
+        console.log(e);
     }
-});
+};
+connect();
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/beefup-db', {
+//     useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// }, (error) => {
+//     if(error){
+//         chalk.red('Failed to connect to the database. Please try again later.')
+//     } else {
+//         chalk.green('Connection Successful.');
+//     }
+// });
